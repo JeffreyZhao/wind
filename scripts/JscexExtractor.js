bb063073b93a4fa626b5b0888e9ca20dcb1d4586
Jeffrey Zhao <jeffz@live.com>
2011-10-23 16:31:54 +0800

    Support "this" and "NEW_WITH_ARGS" in AOT compiler

--- scripts/JscexExtractor.js
+++ scripts/JscexExtractor.js
@@ -67,6 +67,7 @@ JscexExtractor.prototype = {
             case "OBJECT_INIT":
             case "ARRAY_INIT":
             case "PROPERTY_INIT":
+            case "NEW_WITH_ARGS":
             case ".":
             case ">":
             case "<":
@@ -92,6 +93,7 @@ JscexExtractor.prototype = {
             case "null":
             case "true":
             case "false":
+            case "this":
                 break;
             case ";":
                 this._visit(node.expression);
@@ -122,8 +124,8 @@ JscexExtractor.prototype = {
                 this._visit(node.value);
                 break;
             default:
-               debugger;
-               throw new Error('"' + token + '" is not currently supported.');
+                debugger;
+                throw new Error('"' + token + '" is not currently supported.');
         }
     },
 
