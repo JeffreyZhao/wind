b30a40bff9b20254b9e343ac7e9818dcc67f57bc
Jeffrey Zhao <jeffz@live.com>
2011-11-28 01:26:00 +0800

    Add minified version of jscex.async.node.js

--- src/jscex.async.node.js
+++ src/jscex.async.node.js
@@ -71,39 +71,3 @@ Jscex.Async.Node = (function () {
 for (var m in Jscex.Async.Node) {
     exports[m] = Jscex.Async.Node[m];
 }
-
-/*
-Jscex.Async.Node.Path.extend = function (path) {
-
-    path.existsAsync = function (filepath) {
-        var delegate = {
-            "onStart": function (callback) {
-                path.exists(filepath, function (exists) {
-                    callback("success", exists);
-                });
-            }
-        };
-
-        return new Jscex.Async.Task(delegate);
-    };
-}
-
-Jscex.Async.Node.FileSystem.extend = function (fs) {
-
-    fs.readFileAsync = function (filepath) {
-        var delegate = {
-            "onStart": function (callback) {
-                fs.readFile(filepath, function (error, data) {
-                    if (error) {
-                        callback("failure", error);
-                    } else {
-                        callback("success", data);
-                    }
-                });
-            }
-        };
-
-        return new Jscex.Async.Task(delegate);
-    }
-}
-*/
