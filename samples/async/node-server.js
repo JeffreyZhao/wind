6835d41515e72d5dca8a881d2596581f7fa487ff
Jeffrey Zhao <jeffz@live.com>
2011-12-11 18:33:03 +0800

    remove debug code

--- samples/async/node-server.js
+++ samples/async/node-server.js
@@ -41,9 +41,6 @@ var Jscex = require("../../src/jscex-jit");
 require("../../src/jscex-async").init(Jscex);
 var jscexify = require("../../src/jscex-async-node").getJscexify(Jscex);
 
-console.log(path.exists.length);
-console.log(fs.readFile.length);
-
 path.existsAsync = jscexify.fromCallback(path.exists);
 fs.readFileAsync = jscexify.fromStandard(fs.readFile);
 
