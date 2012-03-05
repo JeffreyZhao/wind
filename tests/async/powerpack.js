89cdaf0c6be8451aec505270aded4046263c7ba3
Jeffrey Zhao <jeffz@live.com>
2012-03-05 14:15:51 +0800

    more tests

--- tests/async/powerpack.js
+++ tests/async/powerpack.js
@@ -41,7 +41,7 @@ describe("Task", function () {
     describe("#whenAll()", function () {
 
         it("should directly return an empty object with an empty object input", function () {
-            Task.whenAll({}).start().result.should.eql({});
+            Task.whenAll({ }).start().result.should.eql({ });
         });
 
         it("should directly return an empty array with an empty array input", function () {
@@ -56,7 +56,7 @@ describe("Task", function () {
         });
 
         it("should directly return the error if the task is already faulted", function () {
-            var error = {};
+            var error = { };
             var t = Task.failure(error).start();
             t.error.should.equal(error);
 
@@ -66,10 +66,22 @@ describe("Task", function () {
         it("should return an array of results with a serial of tasks", function (done) {
             this.timeout(100);
 
-            var t1 = Task.delay(10, null, 1);
-            var t2 = Task.delay(10, null, 2);
+            var t1 = Task.delay(0, null, 1);
+            var t2 = Task.delay(0, null, 2);
 
             Task.whenAll(t1, t2).start().addEventListener("success", function () {
+                this.result.should.eql([1, 2]);
+                done();
+            });
+        });
+
+        it("should return an array of result with an array input", function (done) {
+            this.timeout(100);
+
+            var t1 = Task.delay(0, null, 1);
+            var t2 = Task.delay(0, null, 2);
+
+            Task.whenAll([t1, t2]).start().addEventListener("success", function () {
                 this.result.should.eql([1, 2]);
                 done();
             });
@@ -78,11 +90,72 @@ describe("Task", function () {
         it("should return a hash of results with a hash input", function (done) {
             this.timeout(100);
 
-            var t1 = Task.delay(10, null, 1);
-            var t2 = Task.delay(10, null, 2);
+            var t1 = Task.delay(0, null, 1);
+            var t2 = Task.delay(0, null, 2);
 
             Task.whenAll({ r1: t1, r2: t2 }).start().addEventListener("success", function () {
                 this.result.should.eql({ r1: 1, r2: 2 });
+                done();
+            });
+        });
+
+        it("should return the error when one of the task in array is failed", function (done) {
+            this.timeout(100);
+
+            var error = { };
+            var t1 = Task.delay(0, error, null);
+            var t2 = Task.delay(0, null, 1);
+
+            Task.whenAll(t1, t2).start().addEventListener("failure", function () {
+                this.error.should.equal(error);
+                done();
+            });
+        });
+
+        it("should return the error when one of the task in hash is failed", function (done) {
+            this.timeout(100);
+
+            var error = { };
+            var t1 = Task.delay(0, error, null);
+            var t2 = Task.delay(0, null, 1);
+
+            Task.whenAll({ r1: t1, r2: t2 }).start().addEventListener("failure", function () {
+                this.error.should.equal(error);
+                done();
+            });
+        });
+
+        it("should return the object graph the same as the input tasks", function (done) {
+            this.timeout(100);
+
+            var input = {
+                dataList: [
+                    Task.delay(0, null, 1),
+                    {
+                        hello: Task.delay(0, null, "hello"),
+                        world: Task.delay(0, null, "world"),
+                        empty: 10
+                    },
+                    Task.delay(0, null, 2)
+                ],
+                value: Task.delay(0, null, 3),
+                empty: { }
+            };
+
+            Task.whenAll(input).start().addEventListener("success", function () {
+                this.result.should.eql({
+                    dataList: [
+                        1,
+                        {
+                            hello: "hello",
+                            world: "world",
+                            empty: { }
+                        },
+                        2
+                    ],
+                    value: 3,
+                    empty: { }
+                });
                 done();
             });
         });
