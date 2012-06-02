069a9e4d4528893b1cd5a2756b37e66de9b74c46
Jeffrey Zhao <jeffz@live.com>
2012-06-02 15:21:57 +0800

    new doc

---
+++ doc/src/files/scripts/manuals.js
@@ -0,0 +1,57 @@
+"use strict";
+
+document.createElement("header");
+document.createElement("nav");
+document.createElement("aside");
+document.createElement("footer");
+document.createElement("section");
+
+$(document).ready(function () {
+
+    var currentList = $("#toc-list").empty();
+    var currentLevel = 2;
+    
+    var getName = function (h) {
+        var prevP = $(h).prev()[0];
+        if (!prevP) return;
+        if (prevP.tagName.toLowerCase() != "p") return;
+        
+        var link = prevP.firstChild;
+        if (!link) return;
+        if (!link.tagName) return;
+        if (link.tagName.toLowerCase() != "a") return;
+        if (link.innerHTML != "") return;
+        
+        return link.name;
+    }
+    
+    var getLevel = function (h) {
+        return h.tagName.toLowerCase() == "h2" ? 2 : 3;
+    }
+    
+    var getLink = function (h, name) {
+        return $("<a>").attr("href", "#" + name).html($(h).html());
+    }
+    
+    var addToList = function () {
+        var name = getName(this);
+        if (!name) return;
+        
+        var level = getLevel(this);
+        if (level > currentLevel) {
+            var children = currentList.children();
+            var lastItem = children[children.length - 1];
+            
+            var newList = $("<ul>").appendTo(lastItem);
+            currentList = newList;
+        } else if (level < currentLevel) {
+            currentList = currentList.parent().parent();
+        }
+        
+        currentLevel = level;
+        
+        $("<li>").append(getLink(this, name)).appendTo(currentList);
+    }
+    
+    $("#container > h2, #container > h3").each(addToList);
+});
