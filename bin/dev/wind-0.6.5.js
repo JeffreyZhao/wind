d1f19ade8ca89f87dc88875a75128e848a918b0a
Jeffrey Zhao <jeffz@live.com>
2012-08-09 23:47:47 +0800

    rebuild

---
+++ bin/dev/wind-0.6.5.js
@@ -0,0 +1,317 @@
+(function () {
+    "use strict";
+    
+    var Wind;
+    
+    var _ = (function () {
+    
+        var isArray = function (obj) {
+            return Object.prototype.toString.call(obj) === '[object Array]';
+        }
+    
+        var each = function (obj, action) {
+            if (isArray(obj)) {
+                for (var i = 0, len = obj.length; i < len; i++) {
+                    var value = action(i, obj[i]);
+                    if (value !== undefined)
+                        return value;
+                }
+            } else {
+                for (var key in obj) {
+                    if (obj.hasOwnProperty(key)) {
+                        var value = action(key, obj[key]);
+                        if (value !== undefined)
+                            return value;
+                    }
+                }
+            }
+        }
+        
+        var map = function (obj, mapper, valueMapper) {
+            if (isArray(obj)) {
+                var array = new Array(obj.length);
+                for (var i = 0, len = obj.length; i < len; i++) {
+                    array[i] = mapper(obj[i]);
+                }
+                return array;
+            } else {
+                var newObj = {};
+                for (var key in obj) {
+                    if (obj.hasOwnProperty(key)) {
+                        var value = obj[key];
+                        var newKey = mapper ? mapper(key) : key;
+                        var newValue = valueMapper ? valueMapper(value) : value;
+                        newObj[newKey] = newValue;
+                    }
+                }
+                return newObj;
+            }
+        }
+        
+        var v2n = function (version) {
+            var value = 0;
+            
+            var parts = version.split(".");
+            for (var i = 0; i < 3; i++) {
+                value *= 100;
+                if (i < parts.length) {
+                    value += parseInt(parts[i], 10);
+                }
+            }
+            
+            return value;
+        }
+        
+        var testVersion = function (expected, version) {
+            var expectedMinVersion = expected.substring(1); // remove leading "~"
+            
+            var expectedMaxParts = expectedMinVersion.split(".");
+            expectedMaxParts[expectedMaxParts.length - 1] = "0";
+            expectedMaxParts[expectedMaxParts.length - 2] = (parseInt(expectedMaxParts[expectedMaxParts.length - 2], 10) + 1).toString();
+            var expectedMaxVersion = expectedMaxParts.join(".");
+            
+            var versionNumber = v2n(version);
+            return v2n(expectedMinVersion) <= versionNumber && versionNumber < v2n(expectedMaxVersion);
+        }
+        
+        var format = function (f, args) {
+            // support _.format(f, a0, a1, ...);
+            if (!isArray(args)) {
+                var newArgs = new Array(arguments.length - 1);
+                for (var i = 1; i < arguments.length; i++) {
+                    newArgs[i - 1] = arguments[i];
+                }
+                
+                return format(f, newArgs);
+            }
+            
+            return f.replace(/\{{1,2}\d+\}{1,2}/g, function (ph) {
+                if (ph.indexOf("{{") == 0 && ph.indexOf("}}") == ph.length - 2) {
+                    return ph.substring(1, ph.length - 1);
+                }
+                
+                var left = 0;
+                while (ph[left] == "{") left++;
+                
+                var right = ph.length - 1;
+                while (ph[right] == "}") right--;
+                
+                var index = parseInt(ph.substring(left, right + 1), 10);
+                return ph.replace("{" + index + "}", args[index]);
+            });
+        }
+        
+        var once = function (fn) {
+            var called = false;
+            return function () {
+                if (called) return;
+                fn.apply(this, arguments);
+                called = true;
+            }
+        };
+        
+        return {
+            isArray: isArray,
+            each: each,
+            map: map,
+            v2n: v2n,
+            testVersion: testVersion,
+            format: format,
+            once: once
+        };
+    })();
+
+    var Level = {
+        ALL: 0,
+        TRACE: 1,
+        DEBUG: 2,
+        INFO: 3,
+        WARN: 4,
+        ERROR: 5,
+        OFF: 100
+    };
+
+    var Logger = function () {
+        this.level = Level.DEBUG;
+    };
+    Logger.prototype = {
+        log: function (level, msg) {
+            if (this.level <= level) {
+                try { console.log(msg); } catch (ex) { }
+            }
+        },
+
+        trace: function (msg) {
+            this.log(Level.TRACE, msg);
+        },
+
+        debug: function (msg) {
+            this.log(Level.DEBUG, msg);
+        },
+
+        info: function (msg) {
+            this.log(Level.INFO, msg);
+        },
+
+        warn: function (msg) {
+            this.log(Level.WARN, msg);
+        },
+
+        error: function (msg) {
+            this.log(Level.ERROR, msg);
+        }
+    };
+
+    var loadModules = function (require, modules) {
+        if (require) {
+            _.each(modules, function (i, name) {
+                var m;
+                try {
+                    m = require("./wind-" + name);
+                } catch (ex) {
+                    m = require("wind-" + name);
+                }
+                
+                m.init();
+            });
+        } else {
+            _.each(modules, function (i, m) {
+                m.init();
+            });
+        }
+    }
+
+    var initModule = function (options) {
+        var existingModule = Wind.modules[options.name];
+        if (existingModule && existingModule.version != options.version) {
+            Wind.logger.warn(_.format(
+                'The module "{0}" with version "{1}" has already been initialized, skip version "{2}".',
+                options.name,
+                existingModule.version,
+                options.version));
+        }
+    
+        checkDependencies(options);
+        options.init();
+        
+        var module = {};
+        exportBasicOptions(module, options);
+        Wind.modules[options.name] = module;
+    }
+    
+    var checkDependencies = function (options) {
+        if (options.coreDependency && !_.testVersion(options.coreDependency, Wind.coreVersion)) {
+            throw new Error(_.format(
+                'Version of core component mismatched, expected: "{0}", actual: "{1}".',
+                options.coreDependency,
+                Wind.coreVersion));
+            
+        }
+        
+        _.each(options.dependencies || [], function (name, expectedVersion) {
+            var module = Wind.modules[name];
+            if (!module) {
+                throw new Error(_.format(
+                    'Missing required module: "{0}" (expected version: "{1}").',
+                    name,
+                    expectedVersion));
+            }
+
+            if (!_.testVersion(expectedVersion, module.version)) {
+                throw new Error(_.format(
+                    'Version of module "{0}" mismatched, expected: "{1}", actual: "{2}".',
+                    name,
+                    expectedVersion,
+                    module.version));
+            }
+        });
+    }
+    
+    var exportBasicOptions = function (exports, options) {
+        exports.name = options.name;
+        exports.version = options.version;
+        
+        if (options.autoloads) {
+            exports.autoloads = options.autoloads;
+        }
+        
+        if (options.coreDependency) {
+            exports.coreDependency = options.coreDependency;
+        }
+        
+        if (options.dependencies) {
+            exports.dependencies = options.dependencies;
+        }
+    }
+
+    // CommonJS
+    var isCommonJS = !!(typeof require === "function" && typeof module !== "undefined" && module.exports);
+    // CommonJS AMD
+    var isAmd = !!(typeof require === "function" && typeof define === "function" && define.amd);
+    
+    var defineModule = function (options) {
+        var autoloads = options.autoloads || [];
+
+        if (isCommonJS) {
+            exportBasicOptions(options.exports, options);
+            options.exports.init = _.once(function () {
+                loadModules(options.require, autoloads);
+                initModule(options);
+            });
+        } else if (isAmd) {
+            var dependencies = _.map(autoloads, function (name) { return "wind-" + name; });
+            
+            define("wind-" + options.name, dependencies, function () {
+                var loadedModules = arguments;
+                
+                var exports = {};
+                exportBasicOptions(exports, options);
+                exports.init = _.once(function () {
+                    loadModules(null, loadedModules);
+                    initModule(options);
+                });
+
+                return exports;
+            });
+        } else {
+            initModule(options);
+        }
+    }
+
+    var init = function () {
+        Wind.coreVersion = "0.6.5";
+        
+        Wind.logger = new Logger();
+        Wind.Logging = {
+            Logger: Logger,
+            Level: Level
+        };
+
+        Wind._ = _;
+        Wind.modules = { };
+        Wind.binders = { };
+        Wind.builders = { };
+        Wind.define = defineModule;
+    };
+    
+    if (isCommonJS) {
+        Wind = module.exports;
+        init();
+    } else if (isAmd) {
+        define("wind", function () {
+            Wind = { };
+            init();
+            return Wind;
+        });
+    } else {
+        // Get the global object.
+        var Fn = Function, global = Fn('return this')();
+    
+        if (global.Wind) {
+            throw new Error("There's already a Wind root here, please load the component only once.");
+        }
+        
+        Wind = global.Wind = { };
+        init();
+    }
+})();
---
+++ bin/dev/wind-promise-0.6.5.js
@@ -0,0 +1,99 @@
+(function () {
+    "use strict";
+
+    var Wind;
+    
+    var defaultCreate = function () {
+        throw new Error('Please set "Wind.Promise.create" to provide a factory method for creating a promise object.');
+    }
+    
+    var PromiseBuilder = function () { }
+    PromiseBuilder.prototype = {
+        Start: function (_this, task) {
+            return Wind.Promise.create(function (complete, error) {
+                task.next(_this, function (type, value, target) {
+                    if (type == "normal" || type == "return") {
+                        complete(value);
+                    } else if (type == "throw") {
+                        error(value);
+                    } else {
+                        throw new Error("Unsupported type: " + type);
+                    }
+                });
+            });
+        },
+
+        Bind: function (promise, generator) {
+            return {
+                next: function (_this, callback) {
+                    promise.then(function (result) {
+                        var nextTask;
+                        try {
+                            nextTask = generator.call(_this, result);
+                        } catch (ex) {
+                            return callback("throw", ex);
+                        }
+
+                        nextTask.next(_this, callback);
+                    }, function (error) {
+                        callback("throw", error);
+                    });
+                }
+            };
+        }
+    }
+    
+    // CommonJS
+    var isCommonJS = !!(typeof require === "function" && typeof module !== "undefined" && module.exports);
+    // CommonJS AMD
+    var isAmd = !!(typeof require === "function" && typeof define === "function" && define.amd);
+
+    var defineModule = function () {
+        Wind.define({
+            name: "promise",
+            version: "0.6.5",
+            exports: isCommonJS && module.exports,
+            require: isCommonJS && require,
+            autoloads: [ "builderbase" ],
+            dependencies: { builderbase: "~0.6.5" },
+            init: function () {
+                Wind._.each(Wind.BuilderBase.prototype, function (m, fn) {
+                    PromiseBuilder.prototype[m] = fn;
+                });
+            
+                if (!Wind.Promise) {
+                    Wind.Promise = {};
+                }
+                
+                Wind.Promise.create = defaultCreate;
+            
+                Wind.binders["promise"] = "$await";
+                Wind.builders["promise"] = new PromiseBuilder();
+            }
+        });
+    }
+
+    if (isCommonJS) {
+        try {
+            Wind = require("./wind");
+        } catch (ex) {
+            Wind = require("wind");
+        }
+        
+        defineModule();
+    } else if (isAmd) {
+        require(["wind"], function (wind) {
+            Wind = wind;
+            defineModule();
+        });
+    } else {
+        var Fn = Function, global = Fn('return this')();
+
+        if (!global.Wind) {
+            throw new Error('Missing the root object, please load "wind" component first.');
+        }
+        
+        Wind = global.Wind;
+        defineModule();
+    }
+})();
